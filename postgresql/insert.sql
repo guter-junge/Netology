ae297ef5f09f759a251d69d6a8b46aa1fb107706
Elisei <31654801+guter-junge@users.noreply.github.com>
2023-05-23 18:05:16 +0400

    Add files via upload

--- postgresql/insert.sql
+++ postgresql/insert.sql
@@ -7,7 +7,7 @@ values ('Pop'), ('Rap'), ('Experimental'), ('Industrial techno'), ('Soul');
 
 insert into artists_genres(artist_id, genre_id)
 values (1, 1), (1, 5), (2, 1), (3, 1),
-(4, 4), (5, 3), (6, 1), (7, 2), (8, 1), (8, 3)
+(4, 4), (5, 3), (6, 1), (7, 2), (8, 1), (8, 3);
 
 insert into albums(name, release_year)
 values ('Das Ende vom Kreis', 2010), ('Anti', 2016), ('21', 2011), ('Starboy', 2016),
@@ -15,7 +15,7 @@ values ('Das Ende vom Kreis', 2010), ('Anti', 2016), ('21', 2011), ('Starboy', 2
 ('До Свидания', 2020), ('Intro Bonito', 2014);
 
 insert into albums(name, release_year)
-values ('Aus Den Falten', 2018)
+values ('Aus Den Falten', 2018);
 
 insert into albums_artists(artist_id, album_id)
 values (1, 4), (2, 2), (3, 3), (4, 1),
@@ -29,7 +29,7 @@ values ('Id rather sleep', 115, 8), ('Sick Beat', 179, 8), ('Wie ich heis', 236,
 ('Думай позитивно', 221, 6), ('Душно', 209, 6), ('Весело и грустно', 194, 7);
 
 insert into tracks(name, duration, album_id)
-values ('My party', 167, 8)
+values ('My party', 167, 8);
 
 insert into collections(name, release_year)
 values ('Nightsleep', 2022), ('For work', 2023), ('In case of feelings', 2021),
