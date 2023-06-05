e43e7f14e8dbbda66bbc206a7ae87968f031e95c
Elisei <31654801+guter-junge@users.noreply.github.com>
2023-06-05 17:49:20 +0400

    Update python_db.py

--- postgresql/python_db.py
+++ postgresql/python_db.py
@@ -1,24 +1,15 @@
 import psycopg2
 
 
-def create_db(database, user, password):
-    conn = psycopg2.connect(database=database, user=user, password=password)
-    conn.autocommit = True
-    cur = conn.cursor()
-
+def create_db(database, cur):
     try:
-        cur.execute(f'create database {database}')
+        cur.execute('create database ' + database)
         print(f'Database created successfully')
     except Exception as e:
         print('Error creating a database:', str(e))
 
 
-def create_tables(database, user, password):
-    conn = psycopg2.connect(database=database, user=user, password=password)
-    conn.autocommit = True
-    cur = conn.cursor()
-
-
+def create_tables(cur):
     try:
         cur.execute("""
                        create table if not exists users (
@@ -40,15 +31,8 @@ def create_tables(database, user, password):
     except Exception as e:
         print('Error creating tables:', str(e))
 
-def add_client(database, user, password):
-    conn = psycopg2.connect(database=database, user=user, password=password)
-    conn.autocommit = True
-    cur = conn.cursor()
-
+def add_client(cur, first_name, last_name, email):
     try:
-        first_name = input("Enter the client's first name: ")
-        last_name = input("Enter the client's last name: ")
-        email = input("Enter the client's email: ")
         cur.execute("""
                     insert into users(first_name, last_name, email)
                     values (%s, %s, %s)
@@ -58,40 +42,27 @@ def add_client(database, user, password):
         print('Error trying to add a client: ', str(e))
 
 
-def add_phone_number(database, user, password):
-    conn = psycopg2.connect(database=database, user=user, password=password)
-    conn.autocommit = True
-    cur = conn.cursor()
-
+def add_phone_number(cur, user_id, phone_number):
     try:
-        user_id = input("Enter the client's id to add a phone number: ")
-        phone_number = input("Enter the phone number: ")
         cur.execute("""
                     select id from users
                     where id = %s              
                     """, (user_id,))
         result = cur.fetchall()
 
-
         if result:
             cur.execute("""
                         insert into phone_numbers(user_id, phone_number)
                         values (%s, %s)
                         """, (user_id, phone_number))
-            print(f'Phone number added successfully')
+            print(f'Phone number {phone_number} added successfully')
         else:
             print(f'User with {user_id} does not exist')
     except Exception as e:
         print('Error finding a client: ', str(e))
 
-def update_client_info(database, user, password):
-    conn = psycopg2.connect(database=database, user=user, password=password)
-    conn.autocommit = True
-    cur = conn.cursor()
-
+def update_client_info(cur, user_id, column_name, new_value):
     try:
-        user_id = int(input("Enter the client's id for the client you want to update: "))
-
         cur.execute("""
                     select * from users
                     where id = %s
@@ -99,16 +70,12 @@ def update_client_info(database, user, password):
         result = cur.fetchone()
 
         if result:
-            column_name = input('Enter the column name(s) you would like to update separated by comma: ')
-            columns = [col.strip() for col in column_name.split(',')]
-
-            for col in columns:
-                new_value = input(f'Enter the new value for {col}: ')
-                cur.execute(f"""
-                            update users
-                            set {col} = %s
-                            where id = %s
-                            """, (new_value, user_id))
+            query = """
+                    update users
+                    set {} = %s
+                    where id = %s
+                    """.format(column_name)
+            cur.execute(query, (new_value, user_id))
 
             print('Client information updated successfully')
         else:
@@ -116,37 +83,27 @@ def update_client_info(database, user, password):
     except Exception as e:
         print("Error updating client's info: ", str(e))
 
-def delete_phone_number(database, user, password):
-    conn = psycopg2.connect(database=database, user=user, password=password)
-    conn.autocommit = True
-    cur = conn.cursor()
-
+def delete_phone_number(cur, phone_number):
     try:
-        user_id = input("Enter client's id for client for whom you want to delete the phone number: ")
         cur.execute("""
                     select * from phone_numbers
-                    where user_id = %s
-                    """, (user_id,))
+                    where phone_number = %s
+                    """, (phone_number,))
         result = cur.fetchone()
 
         if result:
             cur.execute("""
                         delete from phone_numbers
-                        where user_id = %s
-                        """, (user_id,))
+                        where phone_number = %s
+                        """, (phone_number,))
             print("Client's phone number deleted successfully")
         else:
-            print(f'User with {user_id} does not exist')
+            print(f'Phone_number {phone_number} does not exist')
     except Exception as e:
-        print("Error updating client's info: ", str(e))
+        print("Error deleting a phone number: ", str(e))
 
-def delete_client(database, user, password):
-    conn = psycopg2.connect(database=database, user=user, password=password)
-    conn.autocommit = True
-    cur = conn.cursor()
-
+def delete_client(cur, user_id):
     try:
-        user_id = input('Enter the user_id for client who you wish to delete: ')
         cur.execute("""
                     select * from users
                     where id = %s
@@ -171,21 +128,29 @@ def delete_client(database, user, password):
     except Exception as e:
         print("Error updating client's info: ", str(e))
 
-def find_client(database, user, password):
-    conn = psycopg2.connect(database=database, user=user, password=password)
-    conn.autocommit = True
-    cur = conn.cursor()
-
+def find_client(cur, value):
     try:
-        value = input("Enter client's first name, last name, email or phone number: ")
-        cur.execute("""
+        query =  """
                     select u.*, pn.phone_number from users u
                     left join phone_numbers pn on pn.user_id = u.id
-                    where u.first_name = %s
-                        or u.last_name = %s
-                        or u.email = %s
-                        or pn.phone_number = %s
-                        """, (value, value, value, value))
+                    where 1=1
+                    """
+        parameters = []
+        value_parts = value.split(' ')
+
+        if len(value_parts) >= 1:
+            query += " and (u.first_name = %s or u.last_name = %s or u.email = %s or pn.phone_number = %s)"
+            parameters.extend([value_parts[0], value_parts[0], value_parts[0], value_parts[0]])
+        if len(value_parts) >= 2:
+            query += " and (u.first_name = %s or u.last_name = %s or u.email = %s or pn.phone_number = %s)"
+            parameters.extend([value_parts[1], value_parts[1], value_parts[1], value_parts[1]])
+        if len(value_parts) >= 3:
+            query += " and (u.first_name = %s or u.last_name = %s or u.email = %s or pn.phone_number = %s)"
+            parameters.extend([value_parts[2], value_parts[2], value_parts[2], value_parts[2]])
+        if len(value_parts) >= 4:
+            query += " and (u.first_name = %s or u.last_name = %s or u.email = %s or pn.phone_number = %s)"
+            parameters.extend([value_parts[3], value_parts[3], value_parts[3], value_parts[3]])
+        cur.execute(query, parameters)
         result = cur.fetchall()
 
         if result:
@@ -198,14 +163,39 @@ def find_client(database, user, password):
     except Exception as e:
         print('Error finding a client: ', str(e))
 
-
-
-with psycopg2.connect(database='personal_info', user='postgres', password=password) as conn:
-    # create_db('personal_info', 'postgres', password)
-    # create_tables('personal_info', 'postgres', password)
-    # add_client('personal_info', 'postgres', password)
-    # add_phone_number('personal_info', 'postgres', password)
-    # update_client_info('personal_info', 'postgres', password)
-    # delete_phone_number('personal_info', 'postgres', password)
-    # delete_client('personal_info', 'postgres', password)
-    # find_client('personal_info', 'postgres', password)
+if __name__ == '__main__':
+    with psycopg2.connect(database='personal_info', user='postgres', password='Ulik2054768') as conn:
+        conn.autocommit = True
+        cur = conn.cursor()
+        print('1 = create database, 2 = create tables, \n'
+              '3 = add a client, 4 = add a phone number, \n'
+              '5 = update client info, 6 = delete phone number, \n'
+              '7 = delete client, 8 = find a client')
+        command = (input('Enter a command: ')).lower()
+        if command == '1':
+            create_db('personal_info', cur)
+        elif command == '2':
+            create_tables(cur)
+        elif command == '3':
+            first_name = input("Enter the client's first name: ")
+            last_name = input("Enter the client's last name: ")
+            email = input("Enter the client's email: ")
+            add_client(cur, first_name, last_name, email)
+        elif command == '4':
+            user_id = input("Enter the client's id to add a phone number: ")
+            phone_number = input("Enter the phone number: ")
+            add_phone_number(cur, user_id, phone_number)
+        elif command == '5':
+            user_id = int(input("Enter the client's id for the client you want to update: "))
+            column_name = input('Enter the column name you would like to update: ')
+            new_value = input('Enter the new value for the column: ')
+            update_client_info(cur, user_id, column_name, new_value)
+        elif command == '6':
+            phone_number = input('Enter a phone number you would like to delete: ')
+            delete_phone_number(cur, phone_number)
+        elif command == '7':
+            user_id = input('Enter the user_id for client who you wish to delete: ')
+            delete_client(cur, user_id)
+        elif command == '8':
+            value = input("Enter client's first name, last name, email or phone number: ")
+            find_client(cur, value)
